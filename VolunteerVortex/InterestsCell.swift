e366efe2b2b0f77cd69c4cb9c0a205837eb459d4
amffz9 <aforbis3091@gmail.com>
2015-11-17 13:38:41 -0600

    Added Files
    wrong branch Work

---
+++ VolunteerVortex/InterestsCell.swift
@@ -0,0 +1,24 @@
+//
+//  InterestsCell.swift
+//  VolunteerVortex
+//
+//  Created by MU IT Program on 11/17/15.
+//  Copyright © 2015 George Gilmartin. All rights reserved.
+//
+
+import UIKit
+
+class InterestsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
