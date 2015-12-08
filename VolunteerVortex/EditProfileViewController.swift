8c1f0eff3cd9929d34692dec755bd26c25a4d351
George Gilmartin <gmgq72@mail.missouri.edu>
2015-12-08 16:34:38 -0600

    Scroll View Functionality
    Added in scrollView functionality for Edit Profile View

--- VolunteerVortex/EditProfileViewController.swift
+++ VolunteerVortex/EditProfileViewController.swift
@@ -8,30 +8,33 @@
 
 import UIKit
 
-class EditProfileViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate, UITextFieldDelegate {
+class EditProfileViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var cityNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var stateTextField: UITextField!
+
     
     var activeTextField: UITextField? = nil
-    let keyboardVerticalSpacing: CGFloat = 30
+    let keyboardVerticalSpacing: CGFloat = 0
     
     @IBAction func saveButton(sender: AnyObject) {
         
     }
     
-    @IBOutlet weak var agePicker: UIPickerView!
+    /*@IBOutlet weak var agePicker: UIPickerView!
     let agePickerData = ["18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100", "101", "102", "103", "104", "105", "106", "107", "108", "109", "110", "111", "112", "113", "114", "115", "116", "117", "118", "119", "120", "121", "122"]
     
     @IBOutlet weak var statePicker: UIPickerView!
-    let statePickerData = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
+    let statePickerData = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]*/
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Edit My Profile"
+        self.title = "Edit Profile"
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardDidShowNotification, object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillBeHidden:", name: UIKeyboardWillHideNotification, object: nil)
@@ -45,7 +48,7 @@ class EditProfileViewController: UIViewController,UIPickerViewDataSource,UIPicke
     
     //MARK: - Delegates and data sources
     //MARK: Data Sources
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+    /*func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
         return 1
     }
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
@@ -68,7 +71,7 @@ class EditProfileViewController: UIViewController,UIPickerViewDataSource,UIPicke
     
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         
-    }
+    }*/
 
     func textFieldDidBeginEditing(textField: UITextField) {
         activeTextField = textField
